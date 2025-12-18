88c49b692af1af8170eaab47841f63b94d7ff097
Will Kim <rlagmlckd@gmail.com>
2025-12-18 16:10:43 +0900

    Enabling SSL

--- browser.py
+++ browser.py
@@ -1,14 +1,25 @@
+import sys
 import socket
+import ssl
     
 class URL:
   def __init__(self, url):
     self.scheme, url = url.split("://", 1)
-    assert self.scheme == "http"
+    assert self.scheme in ["http", "https"]
+
+    if self.scheme == "http":
+      self.port = 80
+    elif self.scheme == "https":
+      self.port = 443
 
     if "/" not in url:
       url = url + "/"
     self.host, url = url.split("/", 1)
     self.path = "/" + url
+
+    if ":" in self.host:
+      self.host, port = self.host.split(":", 1)
+      self.port = int(port)
 
   def request(self):
     s = socket.socket(
@@ -17,7 +28,11 @@ class URL:
       proto = socket.IPPROTO_TCP,
     )
 
-    s.connect((self.host, 80))
+    s.connect((self.host, self.port))
+
+    if self.scheme == "https":
+      ctx = ssl.create_default_context()
+      s = ctx.wrap_socket(s, server_hostname=self.host)
 
     request = "GET {} HTTP/1.0\r\n".format(self.path)
     request += "Host: {}\r\n".format(self.host)
@@ -45,20 +60,19 @@ class URL:
     s.close()
     return content
   
-  def show(self, body):
-    in_tag = False
-    for c in body:
-      if c == "<":
-        in_tag = True
-      elif c == ">":
-        in_tag = False
-      elif not in_tag:
-        print(c, end="")
+def show(body):
+  in_tag = False
+  for c in body:
+    if c == "<":
+      in_tag = True
+    elif c == ">":
+      in_tag = False
+    elif not in_tag:
+      print(c, end="")
   
 def load(url):
   body = url.request()
-  url.show(body)
+  show(body)
     
 if __name__ == "__main__":
-    import sys
     load(URL(sys.argv[1]))
