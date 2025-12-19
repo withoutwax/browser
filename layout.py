57eb419bc11cf670e49b737cccf4b6991186fbd7
Will Kim <rlagmlckd@gmail.com>
2025-12-19 17:32:16 +0900

    feat: cache 업데이트

--- layout.py
+++ layout.py
@@ -5,6 +5,17 @@ from parser import Text
 
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
+FONTS = {}
+
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+
+    return FONTS[key][0]
 
 
 class Layout:
@@ -50,11 +61,7 @@ class Layout:
             self.cursor_y += VSTEP
 
     def word(self, word):
-        font = tkinter.font.Font(
-            size=self.size,
-            weight=self.weight,
-            slant=self.style,
-        )
+        font = get_font(self.size, self.weight, self.style)
         w = font.measure(word)
         if self.cursor_x + w > WIDTH - HSTEP:
             self.flush()
